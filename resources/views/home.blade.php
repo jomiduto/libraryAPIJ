b3ce2ca5b43a36f3eece16b90ed568bd6a6dbd81
Jose Miguel Dueñas Torres <jomiguel097@gmail.com>
2024-10-05 19:59:07 -0500

    :tada: Begin a project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Librería AIPJ')
+
+@section('head', 'Acá va el menú')
+
+@section('content')
+    <h2>Contenido</h2>
+    <p>content</p>
+@endsection
+
+@section('footer', 'Acá va el footer')
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Librería AIPJ')</title>
+</head>
+<body>
+    <header>
+        <h1>@yield('head')</h1>
+    </header>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <footer>
+        <p>@yield('footer')</p>
+    </footer>
+</body>
+</html>
