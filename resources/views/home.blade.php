b25bb7955c8a7c0485ae4eadb4f2979423b89e0b
Jose Miguel Dueñas Torres <jomiguel097@gmail.com>
2024-10-08 00:53:22 -0500

    :lipstick: I create the home page, header and footer code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Biblioteca AIPJ | Asociación Iberoamericana de Psicología Jurídica')
 
 @section('content')
-    <main>
+    <main class="mx-auto py-10 w-10/12">
         <section>
-            <h1 class="text-primary font-bold text-[48px]">Biblioteca virtual</h1>
-            <p class="text-lg text-secondary leading-6">La Dirección Editorial de la AIPJ pone a disposición de sus Asociados Recursos Bibliográficos sobre temáticas variadas, bajo el formato de Trabajos técnico-científicos, direccionados a contribuir:</p>
-            <ul class="pl-5 mt-5 list-disc	">
+            <h1 class="font-sora text-primary font-bold text-7xl mb-3">Biblioteca virtual</h1>
+            <p class="mt-3 font-dm-sans text-secondary text-3xl font-medium tracking-wider">La Dirección Editorial de la AIPJ pone a disposición de sus Asociados Recursos Bibliográficos sobre temáticas variadas, bajo el formato de Trabajos técnico-científicos, direccionados a contribuir:</p>
+            <ul class="pt-5 text-2xl text-secondary font-dm-sans font-medium tracking-wider list-disc">
                 <li>A un mejor desempeño profesional en el ámbito Jurídico Forense</li>
                 <li>A la planificación adecuada de procesos de evaluación/valoración psicológico forense</li>
                 <li>A unificar criterios de intervención interdisciplinar éticamente responsables</li>
@@ -16,4 +16,6 @@
     </main>
 @endsection
 
-@section('footer', 'Acá va el footer')
+@section('footer')
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,35 +5,66 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Librería AIPJ')</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Sora:wght@100..800&display=swap" rel="stylesheet">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Sora:wght@100..800&display=swap');
+    </style>
     @vite('resources/css/app.css')
 </head>
-<body class="m-0 p-0 bg-white">
-    <header class="flex items-center bg-[#F8F8F8] pt-3 pr-5 justify-between border-b-[#ddd]">
-        <div class="h-12">
-            <img src="#" alt="Logo AIPJ">
+<body class="min-h-screen flex flex-col p-0 m-0">
+    <header>
+        <div class="container flex flex-row w-8/12 mx-auto">
+            <div class="flex-1" id="logo">
+                <img class="h-20" src="./img/ID-Siglas-Color.jpg" alt="AIPJ Asociacion Iberoamericana de Psicologia Juridica">
+            </div>
+            <nav class="flex-1">
+                <ul class="flex flex-row h-20 items-center justify-end text-lg text-center mr-6 font-bold font-dm-sans">
+                    <li class="menu-item"><a href="https://www.aipjinternacional.com/">Inicio</a></li>
+                    <li class="menu-item"><a href="https://www.aipjinternacional.com/quienes-somos">Quiénes somos</a></li>
+                    <li class="menu-item"><a href="https://www.aipjinternacional.com/eventos">Eventos</a></li>
+                    <li class="menu-item"><a href="https://www.aipjinternacional.com/asociados">Asociados</a></li>
+                    <li class="menu-item"><a href="https://www.aipjinternacional.com/contacto">Contacto</a></li>
+                </ul>
+            </nav>
         </div>
-        <nav>
-            <ul class="flex m-0 p-0 list-none gap-4">
-                <li class="inline">
-                    <a class="text-primary font-bold hover:text-secondary underline" href="#">Inicio</a>
-                    <a class="text-primary font-bold hover:text-secondary underline" href="#">Quiénes somos</a>
-                    <a class="text-primary font-bold hover:text-secondary underline" href="#">Eventos</a>
-                    <a class="text-primary font-bold hover:text-secondary underline" href="#">Asociados</a>
-                    <a class="text-primary font-bold hover:text-secondary underline" href="#">Contacto</a>
-                </li>
-            </ul>
-        </nav>
     </header>
 
     <div class="container">
         @yield('content')
     </div>
 
-    <footer>
-        <p>@yield('footer')</p>
+    <footer class="w-full absolute bottom-0 bg-[#F5CDCD] py-6">
+        <div class="flex flex-row mx-auto">
+            <div class="flex-1" id="logo">
+                <img class="h-20" src="./img/Icon-Salmon.jpg" alt="AIPJ Asociacion Iberoamericana de Psicologia Juridica  logo">
+            </div>
+            <div class="flex-1">
+                <ul class="mr-6 h-20">
+                    <li class="font-dm-sans text-lg text-secondary"><a href="https://www.aipjinternacional.com/quienes-somos">Quiénes somos</a></li>
+                    <li class="font-dm-sans text-lg text-secondary"><a href="https://www.aipjinternacional.com/beneficios">Beneficios</a></li>
+                    <li class="font-dm-sans text-lg text-secondary"><a href="https://www.aipjinternacional.com/asociados">Asociados</a></li>
+                    <li class="font-dm-sans text-lg text-secondary"><a href="https://www.aipjinternacional.com/contacto">Contacto</a></li>
+                    <li class="font-dm-sans text-lg text-secondary"><a href="https://www.aipjinternacional.com/eventos">Eventos</a></li>
+                    <li class="font-dm-sans text-lg text-secondary"><a href="#">Blog</a></li>
+                </ul>
+            </div>
+            <div class="flex-1">
+                <ul class="mr-6 h-20">
+                    <li class="font-dm-sans text-lg text-secondary">Sede central, Madrid</li>
+                    <li class="font-dm-sans text-lg text-secondary">(+54) 900 900 900</li>
+                    <li class="font-dm-sans text-lg text-secondary">info@aipjinternacional</li>
+                </ul>
+            </div>
+        </div>
+        <!-- Social Media Icons -->
+        <div class="container flex justify-center space-x-6 mt-32">
+            <div class="flex justify-center space-x-6">
+                <a href="#" class="hover:text-gray-400">LinkedIn</a>
+                <a href="#" class="hover:text-gray-400">Instagram</a>
+                <a href="#" class="hover:text-gray-400">Facebook</a>
+            </div>
+            <p class="text-center text-primary font-bold font-dm-sans text-lg">© Todos los derechos reservados</p>
+            <p class="mb-2 text-center text-primary font-dm-sans text-lg">Design by <a href="https://www.nowoo.com.co/" target="blank" class="underline">@nöwoo</a></p>
+        </div>
     </footer>
 </body>
 </html>
