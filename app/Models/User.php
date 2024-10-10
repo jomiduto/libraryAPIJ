26d48dcdd62bac4efa83e7b895f2b85cab1209cd
Jose Miguel Dueñas Torres <jomiguel097@gmail.com>
2024-10-09 21:35:22 -0500

    :hammer: Create one to one relation user/country

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Country;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -49,4 +51,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    /**
+     * Get the country associated with the user.
+     */
+    public function country(): HasOne
+    {
+        return $this->hasOne(Country::class);
+    }
+
 }
