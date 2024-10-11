43afca1446f01427c3dbca01c36b7dabec4bc068
Jose Miguel Dueñas Torres <jomiguel097@gmail.com>
2024-10-11 18:09:38 -0500

    :hammer: Create specialization migration

--- database/migrations/2024_10_06_034909_add_fields_to_users_table.php
+++ database/migrations/2024_10_06_034909_add_fields_to_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->date('birthdate');
             $table->string('antiquity', length: 10)->nullable();
             $table->enum('active_payments', ['1', '0'])->nullable();
-            $table->string('specialization', length: 50);
+            $table->integer('specialization')->default(1);
             $table->string('phone', length: 50);
             $table->enum('status_user', ['1', '0'])->default(1);
         });
