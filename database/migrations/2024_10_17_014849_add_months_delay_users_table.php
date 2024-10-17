28fd962d54ab99f38710a94bb8f5801de29be477
Jose Miguel Dueñas Torres <jomiguel097@gmail.com>
2024-10-16 21:34:17 -0500

    :hammer: Create migration months delay

---
+++ database/migrations/2024_10_17_014849_add_months_delay_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('months_delay', length: 3)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('months_delay');
+        });
+    }
+};
